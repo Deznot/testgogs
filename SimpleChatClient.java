7fb1c47f9dcc1f6121c75bfdd01eb542690933a7
Denis Selitsky <denis.selitsky@gmail.com>
2022-09-09 21:35:01 +0300

    feat: client add class go for gui

--- SimpleChatClient.java
+++ SimpleChatClient.java
@@ -17,6 +17,26 @@ public class SimpleChatClient {
     }
 
     public void go() {
-
+        JFrame frame = new JFrame("Simple Chat Client");
+        JPanel mainPanel = new JPanel();
+        incoming = new JTextArea(15,32);
+        incoming.setLineWrap(true);
+        incoming.setWrapStyleWord(true);
+        incoming.setEditable(false);
+        JScrollPane qScroller = new JScrollPane(incoming);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        outgoing = new JTextField(20);
+        JButton sendButton = new JButton("Send");
+        //sendButton.addActionListener(); add method click handler 
+        mainPanel.add(qScroller);
+        mainPanel.add(outgoing);
+        mainPanel.add(sendButton);
+        //setUpNetworking(); method for work with network
+        
+        frame.getContentPane().add(BorderLayout.CENTER,mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,500);
+        frame.setVisible(true);
     }   
 }
